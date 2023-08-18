9b611574f7ee868c9b67f16b1f839c80071a6274
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-08-18 09:35:34 +0000

    p1

--- src/components/App.js
+++ src/components/App.js
@@ -25,14 +25,14 @@ const title = 'Select the gradient and then the Box to change the color';
 
 const App = () => {
   let [nextBackground, selectNextBackground] = useState({ background: "" })
-  const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground)
-  }
+  // const applyColor = (updateSelectionStyle) => {
+  //   updateSelectionStyle(nextBackground);
+  // }
+  console.log(nextBackground)
 
   return (
     <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
-
+      <h5 className="heading">{title}</h5>
       <div className="row">
         {colourConfig.map((config, index) => (
           <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
@@ -42,7 +42,7 @@ const App = () => {
       <div className='row' id="children-wrapper">
         {
           ["selection1", "selection2", "selection3"].map(key => (
-            <Selection key={key} applyColor={applyColor} />
+            <Selection key={key} boxkey={key} nextBackground={nextBackground} />
           ))
         }
       </div>
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React, { useState } from "react";
+import "../styles/Child.css"
 
+const Selection=({boxkey,nextBackground})=>{
+    let [selectedbackground,setSelectedbackground]=useState({background:""});
+    
+    function updatecolor() {
+        setSelectedbackground({background:nextBackground.background})
+    }
+     
+    return(<div onClick={updatecolor} style={selectedbackground.background ? {backgroundColor:selectedbackground.background}: {}} className="fix-box">
+      <h2 className=".subheading">Selection</h2>
+    </div>)
+}
+
+export default Selection;
